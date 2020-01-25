de659b5d59ac47d600afcc9a68ad876b9e399b33
Hidayathulla M A <hidayathullam@ispace.com>
2020-01-25 15:52:10 +0530

    MyThirdCommit2

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var appInsights = require('applicationinsights');
+appInsights.setup('251a529d-f34b-4fba-bb9b-a5fcccdf855e');
+appInsights.start();
+
 var app = express();
 
 // view engine setup
@@ -27,12 +31,16 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+app.use('/problem', function(){
+  throw new Error('Something went wrong with problem');
+})
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  appInsights.defaultClient.trackException({exception:err});
   // render the error page
   res.status(err.status || 500);
   res.render('error');
