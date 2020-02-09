7169b12e046a6f3d4993d303aad88ff25d404dcd
hidayath <hidayathulla.md@ispace.com>
2020-02-09 16:19:23 +0530

    initalcommit

--- Powershell/MyFirstPS.ps1
+++ Powershell/MyFirstPS.ps1
@@ -4,3 +4,6 @@ Connect-AzAccount
 Get-AzResourceGroup
 New-AzResourceGroup -Name ps-res-grp -Location southeastasia
 New-AzResourceGroupDeployment -Name mydeployment -ResourceGroupName ps-res-grp -TemplateFile C:\az_203\MyResGrp_25Jan\linuxtemplate.json -TemplateParameterFile C:\az_203\MyResGrp_25Jan\linuxparameters.json
+
+New-AzResourceGroup -Name iaas-grp -Location southeastasia
+New-AzResourceGroupDeployment -Name mydeployment -ResourceGroupName iaas-grp -TemplateFile C:\az_203\azuredepoly.json
--- app.js
+++ app.js
@@ -7,10 +7,6 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var appInsights = require('applicationinsights');
-appInsights.setup('251a529d-f34b-4fba-bb9b-a5fcccdf855e');
-appInsights.start();
-
 var app = express();
 
 // view engine setup
@@ -26,15 +22,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.use('/problem', function(){
-  throw new Error('Something went wrong with problem');
-});
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -42,7 +33,6 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  appInsights.defaultClient.trackException({exception:err});
   // render the error page
   res.status(err.status || 500);
   res.render('error');
